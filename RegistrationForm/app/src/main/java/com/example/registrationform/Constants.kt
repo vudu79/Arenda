869f7a2965f94ca2050f7daa074e2b197ab84bb1
vudu79 <33384007+vudu79@users.noreply.github.com>
2023-03-29 20:55:13 +0200

    1

---
+++ RegistrationForm/app/src/main/java/com/example/registrationform/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.registrationform
+
+class Constants {
+    companion object {
+        const val SING_STATE = "empty"
+        const val SING_IN = "SING_IN"
+        const val SING_UP = "SING_UP"
+    }
+}
---
+++ RegistrationForm/app/src/main/java/com/example/registrationform/RegisterAvtivity.kt
@@ -0,0 +1,11 @@
+package com.example.registrationform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterAvtivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_avtivity)
+    }
+}
