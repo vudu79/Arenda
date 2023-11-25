0b51adf2de8171198f932219052df75cf6d65fe7
vudu79 <33384007+vudu79@users.noreply.github.com>
2023-11-25 21:46:25 +0200

    начал делать экран для учета баланса

---
+++ NavigationExample/app/src/main/java/com/example/navigationexample/presentation/screens/BalanceViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.navigationexample.presentation.screens
+
+
+import androidx.compose.foundation.gestures.DraggableState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.navigationexample.data.repository.ClientsRepositoryImpl
+import com.example.navigationexample.data.repository.DaysRepositoryImpl
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class BalanceViewModel @Inject constructor(
+    private val clientRepository: ClientsRepositoryImpl,
+    private val daysRepository: DaysRepositoryImpl,
+
+) : ViewModel() {
+    private val _tabIndex: MutableLiveData<Int> = MutableLiveData(0)
+    val tabIndex: LiveData<Int> = _tabIndex
+    val tabs = listOf("Home", "About")
+
+    var isSwipeToTheLeft: Boolean = false
+    private val draggableState = DraggableState { delta ->
+        isSwipeToTheLeft = delta > 0
+    }
+
+    private val _dragState = MutableLiveData<DraggableState>(draggableState)
+    val dragState: LiveData<DraggableState> = _dragState
+
+    fun updateTabIndexBasedOnSwipe() {
+        _tabIndex.value = when (isSwipeToTheLeft) {
+            true -> Math.floorMod(_tabIndex.value!!.plus(1), tabs.size)
+            false -> Math.floorMod(_tabIndex.value!!.minus(1), tabs.size)
+        }
+    }
+
+    fun updateTabIndex(i: Int) {
+        _tabIndex.value = i
+    }
+
+}
