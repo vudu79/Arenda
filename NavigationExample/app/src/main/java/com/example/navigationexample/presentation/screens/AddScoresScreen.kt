acd688adb021d32298c3b19ffcb54a7026aee724
vudu79 <33384007+vudu79@users.noreply.github.com>
2023-12-11 23:52:17 +0200

    начал делать экран добавления доходов1

--- NavigationExample/app/src/main/java/com/example/navigationexample/presentation/screens/AddScoresScreen.kt
+++ NavigationExample/app/src/main/java/com/example/navigationexample/presentation/screens/AddScoresScreen.kt
@@ -1,52 +1,28 @@
 package com.example.navigationexample.presentation.screens
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat.startActivity
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-//import com.example.composeex.ClientItemRow
-import com.example.navigationexample.R
-import com.example.navigationexample.data.entity.Client
-import com.example.navigationexample.presentation.navigation.Routs
-import com.example.navigationexample.presentation.screens.common.CustomAlertDialog
 import com.example.navigationexample.presentation.screens.common.rand
-import com.kizitonwose.calendar.sample.compose.clickable
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Random
 
 
 @Composable
@@ -67,28 +43,42 @@ fun AddScoresScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(red = 41, green = 41, blue = 41))
-            .border(2.dp, color = Color.Gray),
+            .background(color = Color(red = 41, green = 41, blue = 41)),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Top
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp)
                 .background(color = Color(red = 41, green = 41, blue = 41))
-                .border(2.dp, color = Color.Gray),
+//                .border(2.dp, color = Color.Gray)
+            ,
 
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-            androidx.compose.material3.Text(
-                text = "Размер", fontSize = 25.sp, color = Color(223, 75, 0),
+            Divider(
+//                color = Color(223, 75, 0),
+                modifier = Modifier
+                    .fillMaxWidth()  //fill the max height
+                    .height(5.dp)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFF292929),
+                                Color(0xFFF81403),
+                                Color(0xFF292929),
+                            )
+                        )
+                    )
+            )
+            Text(
+                text = "Размер", fontSize = 20.sp, color = Color(223, 75, 0),
                 modifier = Modifier
                     .padding(10.dp)
             )
             OutlinedTextField(
-                shape = RoundedCornerShape(20.dp),
+                shape = RoundedCornerShape(25.dp),
                 value = "",
                 onValueChange = {},
                 placeholder = { },
@@ -96,44 +86,63 @@ fun AddScoresScreen(
                 singleLine = true,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 5.dp, start = 15.dp, end = 15.dp),
+                    .padding(bottom = 20.dp, start = 45.dp, end = 45.dp),
                 colors = TextFieldDefaults.outlinedTextFieldColors(
-                    unfocusedBorderColor = Color.Black,
+                    focusedBorderColor = Color(223, 75, 0).copy(alpha = ContentAlpha.disabled),
+                    unfocusedBorderColor = Color(223, 75, 0).copy(alpha = ContentAlpha.high),
+                    disabledBorderColor = Color(223, 75, 0).copy(alpha = ContentAlpha.medium),
                     textColor = Color.Black,
-                    backgroundColor = Color(142, 143, 138)
+                    backgroundColor = Color(red = 41, green = 41, blue = 41)
                 ),
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Next, keyboardType = KeyboardType.Text,
                     capitalization = KeyboardCapitalization.None,
                     autoCorrect = true,
                 ),
-                keyboardActions = KeyboardActions(onNext = {
-                    focusManager.moveFocus(FocusDirection.Down)
-                },
-                    ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Down)
+                    },
+                ),
+            )
+            Divider(
+//                color = Color(223, 75, 0),
+                modifier = Modifier
+                    .fillMaxWidth()  //fill the max height
+                    .height(5.dp)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFF292929),
+                                Color(0xFFF81403),
+                                Color(0xFF292929),
+                            )
+                        )
+                    )
             )
         }
 
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(10.dp)
+                .padding(bottom = 25.dp, start = 15.dp, end = 15.dp)
                 .background(color = Color(red = 41, green = 41, blue = 41))
-                .border(2.dp, color = Color.Gray),
+//                .border(2.dp, color = Color.Gray)
+            ,
 
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             Text(
-                text = "Категория", fontSize = 25.sp, color = Color(223, 75, 0),
+                text = "Категория", fontSize = 20.sp, color = Color(223, 75, 0),
                 modifier = Modifier
                     .padding(10.dp)
             )
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.4f)
-                    .padding(3.dp)
+                    .fillMaxHeight(0.40f)
+                    .padding(bottom = 20.dp, start = 15.dp, end = 15.dp)
             ) {
                 items(listMock) { item ->
                     CategoryItemRow(
@@ -141,6 +150,21 @@ fun AddScoresScreen(
                     )
                 }
             }
+            Divider(
+//                color = Color(223, 75, 0),
+                modifier = Modifier
+                    .fillMaxWidth()  //fill the max height
+                    .height(5.dp)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Color(0xFF292929),
+                                Color(0xFFF81403),
+                                Color(0xFF292929),
+                            )
+                        )
+                    )
+            )
         }
     }
 }
@@ -154,7 +178,7 @@ fun LazyItemScope.CategoryItemRow(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 15.dp, end= 15.dp, top = 5.dp, bottom=5.dp),
+            .padding(start = 15.dp, end = 15.dp, top = 5.dp, bottom = 5.dp),
         shape = RoundedCornerShape(20.dp),
         elevation = 8.dp,
     ) {
@@ -195,21 +219,22 @@ fun LazyItemScope.CategoryItemRow(
             Column(
                 horizontalAlignment = Alignment.Start,
                 modifier = Modifier
-                    .fillMaxWidth(0.8f)
+                    .fillMaxWidth()
                     .padding(),
             ) {
                 Row(
                     modifier = Modifier
-                        .height(IntrinsicSize.Min)
+                        .height(50.dp)
+//                        .height(IntrinsicSize.Min)
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Start,
+                    horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.CenterVertically
 
                 ) {
                     Text(
                         text = categoryName, fontSize = 20.sp, color = Color.Black,
                         modifier = Modifier
-                            .padding(15.dp)
+                            .padding(5.dp)
                     )
                 }
             }
