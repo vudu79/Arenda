4cbc4ebf3f66f362b0af067735056ebf19b143b2
vudu79 <33384007+vudu79@users.noreply.github.com>
2023-12-12 23:14:48 +0200

    начал делать пейджер для категорий

--- NavigationExample/app/src/main/java/com/example/navigationexample/presentation/screens/AddScoresScreen.kt
+++ NavigationExample/app/src/main/java/com/example/navigationexample/presentation/screens/AddScoresScreen.kt
@@ -1,10 +1,9 @@
 package com.example.navigationexample.presentation.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -23,11 +23,20 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.navigationexample.presentation.screens.common.rand
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
+@OptIn(ExperimentalPagerApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun AddScoresScreen(
 ) {
+    val context = LocalContext.current
     val focusManager = LocalFocusManager.current
     val listMock = listOf<Pair<String, Color>>(
         Pair("Газ", Color(red = rand(0, 255), rand(0, 255), rand(0, 255))),
@@ -127,21 +136,39 @@ fun AddScoresScreen(
                     .padding(top = 10.dp, bottom = 10.dp)
             )
 
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.38f)
-                    .padding(bottom = 20.dp)
-            ) {
-                items(listMock) { item ->
-                    CategoryItemRow(
-                        item.first, item.second
-                    )
-                }
-            }
+            customPagerView(listMock)
+
+//            Box(
+//                modifier = Modifier.fillMaxSize(),
+//                contentAlignment = Alignment.Center
+//            ) {
+//                EndlessHorizontalPager(
+//                    items = listOf("A", "B", "C"),
+//                    itemClicked = {
+//                        Toast.makeText(
+//                            context,
+//                            "item clicked: $it",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                    })
+//            }
+
+
+//            LazyColumn(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .fillMaxHeight(0.38f)
+//                    .padding(bottom = 20.dp)
+//            ) {
+//                items(listMock) { item ->
+//                    CategoryItemRow(
+//                        item.first, item.second
+//                    )
+//                }
+//            }
 
             Button(
-                colors=ButtonDefaults.buttonColors(
+                colors = ButtonDefaults.buttonColors(
                     backgroundColor = Color(red = 41, green = 41, blue = 41),
                     contentColor = Color(223, 75, 0),
                 ),
@@ -150,13 +177,13 @@ fun AddScoresScreen(
                 modifier = Modifier
                     .fillMaxWidth()
 
-                    .padding(bottom = 20.dp),
+                    .padding(bottom = 20.dp,top = 20.dp),
                 border = BorderStroke(1.dp, Color(223, 75, 0)),
                 contentPadding = PaddingValues(10.dp),
                 onClick = { /*TODO*/ }
 
             ) {
-                Text(text = "Добавить другую", fontSize = 20.sp)
+                Text(text = "Добавить категорию", fontSize = 20.sp)
             }
 
             Divider(
@@ -179,11 +206,9 @@ fun AddScoresScreen(
 }
 
 
-
-
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun LazyItemScope.CategoryItemRow(
+fun CategoryItemRow(
     categoryName: String, categoryColor: Color
 ) {
 
@@ -191,7 +216,7 @@ fun LazyItemScope.CategoryItemRow(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 5.dp, bottom = 5.dp),
-        shape = RoundedCornerShape(20.dp),
+        shape = RoundedCornerShape(25.dp),
         elevation = 8.dp,
     ) {
 
@@ -252,6 +277,104 @@ fun LazyItemScope.CategoryItemRow(
         }
     }
 }
+
+
+@OptIn(ExperimentalPagerApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun customPagerView(list: List<Pair<String, Color>>) {
+    val pageState = rememberPagerState()
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
+            HorizontalPager(
+                count = Int.MAX_VALUE,
+                contentPadding = PaddingValues(horizontal = 1.dp),
+                state = pageState,
+                itemSpacing = 5.dp,
+                modifier = Modifier
+                    .background(Color(41, green = 41, blue = 41))
+            ) { page ->
+                list.getOrNull(
+                    (page % list.size)
+                )?.let { item ->
+                    CategoryItemRow(item.first, item.second)
+                }
+            }
+        }
+    }
+
+
+    LaunchedEffect(key1 = Unit, block = {
+        var initPage = Int.MAX_VALUE /2
+        while (initPage % list.size !=0){
+            initPage++
+        }
+        pageState.scrollToPage(initPage)
+    })
+
+
+    LaunchedEffect(key1 = pageState.currentPage, block = {
+        launch {
+            while (true){
+                delay(2500)
+                withContext(NonCancellable){
+                    if(pageState.currentPage + 1 in 0..Int.MAX_VALUE){
+                        pageState.animateScrollToPage(pageState.currentPage +1)
+                    } else{
+                        val initPage = Int.MAX_VALUE /2
+                        pageState.scrollToPage(initPage)
+
+                    }
+                }
+            }
+        }
+    })
+}
+
+
+
+//
+//@OptIn(ExperimentalFoundationApi::class, ExperimentalPagerApi::class)
+//@Composable
+//fun EndlessHorizontalPager(
+//    items: List<String>,
+//    itemClicked: (String) -> Unit
+//) {
+//    val pageCount = Int.MAX_VALUE
+//    val pagerState = rememberPagerState(
+//        initialPage = pageCount / 2
+//    )
+//
+//    HorizontalPager(
+//        modifier = Modifier
+//            .background(Color(0xff469597))
+//            .size(200.dp)
+//            .clickable {
+//                val clickedItem = items[pagerState.currentPage % items.size]
+//                itemClicked(clickedItem)
+//            },
+//        count = pageCount,
+//        state = pagerState
+//    ) { page ->
+//        Box(
+//            modifier = Modifier.fillMaxSize(),
+//            contentAlignment = Alignment.Center
+//        ) {
+//            Text(
+//                text = items[page % items.size],
+//                style = MaterialTheme.typography.h1
+//            )
+//        }
+//    }
+//}
+
 
 
 @Preview
