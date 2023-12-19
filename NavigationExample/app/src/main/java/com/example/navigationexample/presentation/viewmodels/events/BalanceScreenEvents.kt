86a0f7d0ec50402e0d0bad47bc303e463f4c6e26
vudu79 <33384007+vudu79@users.noreply.github.com>
2023-12-19 14:32:54 +0200

    доработать скафолд12

--- NavigationExample/app/src/main/java/com/example/navigationexample/presentation/viewmodels/events/BalanceScreenEvents.kt
+++
@@ -1,5 +0,0 @@
-package com.example.navigationexample.presentation.viewmodels.events
-
-sealed class BalanceScreenEvents {
-    data class onApartmentCheckboxSelected(var apartmentList: List<String>): BalanceScreenEvents()
-}
